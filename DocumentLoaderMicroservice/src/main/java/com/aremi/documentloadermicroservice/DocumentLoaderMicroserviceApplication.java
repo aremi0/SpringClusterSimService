08f3b16bf0f71123859256393135e9e631bec5fd
aremi0 <gabriele.passarello98@gmail.com>
2025-03-16 13:32:21 +0100

    Inizializzazione modulo Spring

---
+++ DocumentLoaderMicroservice/src/main/java/com/aremi/documentloadermicroservice/DocumentLoaderMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aremi.documentloadermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentLoaderMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentLoaderMicroserviceApplication.class, args);
+	}
+
+}
---
+++ DocumentLoaderMicroservice/src/test/java/com/aremi/documentloadermicroservice/DocumentLoaderMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aremi.documentloadermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentLoaderMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
